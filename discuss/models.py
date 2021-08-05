8b11474cf01a0cc4a2f281ac7db9ef0097086f8c
aditya-aot <aditya.aot.11@gmail.com>
2021-08-05 10:04:43 +0530

    discuss part 1

--- discuss/models.py
+++ discuss/models.py
@@ -3,3 +3,8 @@ from django.conf import settings
 from django.contrib.auth.models import  User, auth
 
 # Create your models here.
+
+
+class chat(models.Model):
+     user = models.ForeignKey(User, on_delete=models.CASCADE , null= True  )
+   
