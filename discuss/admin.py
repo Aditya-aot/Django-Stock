14045a63582e6214746084223347b201a5d92a78
aditya-aot <aditya.aot.11@gmail.com>
2021-08-06 14:30:01 +0530

    discuss part 1

--- discuss/admin.py
+++ discuss/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import stock_model , chat
 # Register your models here.
+
+
+
+class Admin(admin.ModelAdmin) :
+    # inlines = [Like_Admin]
+    list_display = ['__str__', 'user']
+    search_fields = ['content','user__username' , 'user__email']
+    class Meta :
+        model = chat
+
+admin.site.register(chat,Admin)
+
+
+admin.site.register(stock_model)
+
--- discuss/models.py
+++ discuss/models.py
@@ -8,8 +8,11 @@ class stock_model(models.Model):
     stock = models.TextField()
 
 class chat(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE , null= True  )
+    chatt = models.ForeignKey(stock_model ,related_name="comments", on_delete= models.CASCADE , null= True)
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE , null= True  )
     pub_date = models.DateTimeField(auto_now=False, auto_now_add=True, null=True)
     content = models.TextField()
     
+    def __aiter__(self):
+        return "%s - %s" % (self.chat.content , self.name)
