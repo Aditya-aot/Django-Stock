e0c956af40190619a82580c101a14e840e057380
aditya-aot <aditya.aot.11@gmail.com>
2021-08-08 13:21:20 +0530

    discuss part 1

--- discuss/admin.py
+++ discuss/admin.py
@@ -4,14 +4,15 @@ from .models import stock_model , chat
 
 
 
-class Admin(admin.ModelAdmin) :
-    # inlines = [Like_Admin]
-    list_display = ['__str__', 'user']
-    search_fields = ['content','user__username' , 'user__email']
-    class Meta :
-        model = chat
+# class Admin(admin.ModelAdmin) :
+#     # inlines = [Like_Admin]
+#     list_display = ['__str__', 'user']
+#     search_fields = ['content','user__username' , 'user__email']
+#     class Meta :
+#         model = chat
 
-admin.site.register(chat,Admin)
+# admin.site.register(chat,Admin)
+admin.site.register(chat)
 
 
 admin.site.register(stock_model)
