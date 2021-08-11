ae9fe50eae94d85eb92ab6006a599ca0f562a443
aditya-aot <aditya.aot.11@gmail.com>
2021-08-11 10:43:19 +0530

    discuss part 1

--- discuss_part2/views.py
+++ discuss_part2/views.py
@@ -2,29 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from .forms import search_day
+# from .forms import search_day
 # Create your views here.
 
 
 def discuss(request) :
 
-    form = search_day()
+    # form = search_day()
 
     a=['a','b','c','d','c']
-    if request.method =='POST' :
-        form = search_day(request.POST, request.FILES)
-        if form.is_valid() :
-            stock = form.cleaned_data['stock']
+    # if request.method =='POST' :
+    #     form = search_day(request.POST, request.FILES)
+    #     if form.is_valid() :
+    #         stock = form.cleaned_data['stock']
 
-        context = {'a':a ,
-                    'form':form ,
-                    'stock' : stock ,
-                        }
-        return render(request, 'discuss/discuss.html',context)
+    #     context = {'a':a ,
+    #                 'form':form ,
+    #                 'stock' : stock ,
+    #                     }
+    #     return render(request, 'discuss/discuss.html',context)
         
-    context = {'a':a ,
-                    'form':form ,
-                        }
+    # context = {'a':a ,
+    #                 'form':form ,
+    #                     }
     return render(request, 'discuss/discuss.html',context) 
 
 
